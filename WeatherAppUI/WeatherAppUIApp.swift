1fd6ffed1876a850ffe41e47265ee81c803d7be2
Anosh Taraporevala <anoshtaraporevala@gmail.com>
2023-10-20 18:18:55 -0700

    Initial Commit

---
+++ WeatherAppUI/WeatherAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppUIApp.swift
+//  WeatherAppUI
+//
+//  Created by Anosh Taraporevala on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
